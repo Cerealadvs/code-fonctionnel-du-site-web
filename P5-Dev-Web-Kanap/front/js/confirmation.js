a5cccaaf4ea0f41c1e6f8838af86b6e94d86a272
cerealadvs <cyrildelteil@gmail.com>
2022-07-06 10:40:22 +0200

    commentaires confirmation

--- P5-Dev-Web-Kanap/front/js/confirmation.js
+++ P5-Dev-Web-Kanap/front/js/confirmation.js
@@ -1,9 +1,11 @@
-//PAGE CONFIRMATION = afficher l'id de la commande.
-//Récupération de l'url de la page commande.
+//PAGE CONFIRMATION = Afficher l'id de la commande.
 const params = window.location.href;
+
 const orderUrl = new URL(params);
-//Récupération orderId dans l'url
+  //Récupération de l'url de la page commande.
+
 const orderId = orderUrl.searchParams.get("id");
+  //Récupération orderId dans l'url.
 
 document.querySelector("#orderId").innerHTML = orderId;
 console.log(orderId)
