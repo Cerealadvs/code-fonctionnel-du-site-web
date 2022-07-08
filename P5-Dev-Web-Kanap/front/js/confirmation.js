5d0f212e3c6a73ebb84013ef55d2ee9f338b2456
cerealadvs <cyrildelteil@gmail.com>
2022-07-08 09:28:45 +0200

    reindent lines harmonisation

--- P5-Dev-Web-Kanap/front/js/confirmation.js
+++ P5-Dev-Web-Kanap/front/js/confirmation.js
@@ -2,10 +2,10 @@
 const params = window.location.href;
 
 const orderUrl = new URL(params);
-  //Récupération de l'url de la page commande.
+//Récupération de l'url de la page commande.
 
 const orderId = orderUrl.searchParams.get("id");
-  //Récupération orderId dans l'url.
+//Récupération orderId dans l'url.
 
 document.querySelector("#orderId").innerHTML = orderId;
 console.log(orderId)
--- P5-Dev-Web-Kanap/front/js/script.js
+++ P5-Dev-Web-Kanap/front/js/script.js
@@ -1,30 +1,30 @@
 fetch('http://localhost:3000/api/products')
 //On fait un appel feetch à l'API pour récupérer les produits, qui nous renvoi une promesse.
 
-    .then(function (reponse) {
-        return reponse.json();
-          //On formate la réponse en json pour pouvoir l'exploiter.
-    })
-    .then(function (products) {
-        //On récupère le résultat de la promesse précédente (products).
-        console.log(products);
-          //On crée une boucle for pour parcourir les données du tableau et les insérer dans le HTML dynamiquement avec innerHTML.
-        
-        for (let product of products) {
-          //À chaque tour de boucle on vient completer le HTML avec les informations avec l'index correspondant.
-            document.getElementById('items').innerHTML += 
-            `<a href="./product.html?id=${product._id}"> 
-              <article>
-                <img src="${product.imageUrl}" alt="${product.altTxt}">
-                <h3 class="productName">${product.name}</h3>
-                <p class="productDescription">${product.description}</p>
-              </article>
-            </a>`;
-        }
-    })
-    
-    .catch(function (err) {
-      //Si la promesse n'est pas résolue on affiche ici un message d'erreur.
-        alert("Impossible d'afficher les produits");
-        console.log(err);
-    });
+.then(function (reponse) {
+  return reponse.json();
+  //On formate la réponse en json pour pouvoir l'exploiter.
+})
+.then(function (products) {
+  //On récupère le résultat de la promesse précédente (products).
+  console.log(products);
+  //On crée une boucle for pour parcourir les données du tableau et les insérer dans le HTML dynamiquement avec innerHTML.
+  
+  for (let product of products) {
+    //À chaque tour de boucle on vient completer le HTML avec les informations avec l'index correspondant.
+    document.getElementById('items').innerHTML += 
+    `<a href="./product.html?id=${product._id}"> 
+    <article>
+    <img src="${product.imageUrl}" alt="${product.altTxt}">
+    <h3 class="productName">${product.name}</h3>
+    <p class="productDescription">${product.description}</p>
+    </article>
+    </a>`;
+  }
+})
+
+.catch(function (err) {
+  //Si la promesse n'est pas résolue on affiche ici un message d'erreur.
+  alert("Impossible d'afficher les produits");
+  console.log(err);
+});
